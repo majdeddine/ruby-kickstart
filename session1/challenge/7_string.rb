798f30fedc78eb0f242c97a463ab5c8bb68dda7c
majdeddine <majda-jambouri@hotmail.fr>
2017-08-08 23:54:13 +0100

    Solution for 7_string

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,11 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  arr=[]
+  (0..string.length-1).each{|x| arr.push(string[x+1]) if string[x] == "r" || string[x] == "R"}
+  arr.join
 end
